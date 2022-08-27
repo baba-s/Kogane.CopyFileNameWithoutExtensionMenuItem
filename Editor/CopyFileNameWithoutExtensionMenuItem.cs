7529853d1caf1ce20cb9ec3f89433673267c629a
baba-s <shotababa1206@gmail.com>
2022-08-27 09:40:15 +0900

    first commit

---
+++ Editor/CopyFileNameWithoutExtensionMenuItem.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    internal static class CopyFileNameWithoutExtensionMenuItem
+    {
+        [Shortcut( "Kogane/Copy File Name Without Extension", KeyCode.C, ShortcutModifiers.Action | ShortcutModifiers.Shift )]
+        private static void Copy()
+        {
+            var activeObject = Selection.activeObject;
+
+            if ( activeObject == null ) return;
+
+            var assetPath = AssetDatabase.GetAssetPath( activeObject );
+
+            if ( string.IsNullOrWhiteSpace( assetPath ) ) return;
+
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension( assetPath );
+
+            EditorGUIUtility.systemCopyBuffer = fileNameWithoutExtension;
+
+            Debug.Log( $"Copied! `{fileNameWithoutExtension}`" );
+        }
+    }
+}
